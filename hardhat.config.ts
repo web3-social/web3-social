db00d68de02bf134295dee91be28f002c9329045
lightsing <light.tsing@gmail.com>
2022-11-29 19:24:41 +0800

    add gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  gasReporter: {
+    enabled: true,
+    currency: 'USD',
+    coinmarketcap: '***REMOVED***',
+    token: 'MATIC',
+    gasPriceApi: 'https://api.polygonscan.com/api?module=proxy&action=eth_gasPrice',
+  }
 };
 
 export default config;
