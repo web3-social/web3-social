dc44f59be691e71af2ba106c57c3191d51d15a36
lightsing <light.tsing@gmail.com>
2022-11-30 22:21:29 +0800

    add hardhat web3

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-web3";
 import "hardhat-gas-reporter";
 
 const config: HardhatUserConfig = {
