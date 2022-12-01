ce029cae063d393f5f371b6ff38984b8b3e1260d
lightsing <light.tsing@gmail.com>
2022-12-01 15:11:22 +0800

    using dotenv

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -2,15 +2,18 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-web3";
 import "hardhat-gas-reporter";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   gasReporter: {
     enabled: true,
     currency: 'USD',
-    coinmarketcap: '***REMOVED***',
-    token: 'MATIC',
-    gasPriceApi: 'https://api.polygonscan.com/api?module=proxy&action=eth_gasPrice',
+    coinmarketcap: process.env.COINMARKETCAP_KEY,
+    token: 'DAI',
+    gasPriceApi: `https://api.gnosisscan.io/api?module=proxy&action=eth_gasPrice&apiKey=${process.env.ETHERSCAN_API_KEY}`,
   }
 };
 
